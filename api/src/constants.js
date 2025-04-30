5d2cf82280bea0dcd63f1745d77c8a5245e175e7
Yash <yashpadhiyar.pyr@gmail.com>
2025-05-01 00:06:10 +0530

    Add form field datatype constants

--- api/src/constants.js
+++ api/src/constants.js
@@ -1,1 +1,51 @@
 export const DB_NAME = "campusbuzz"
+
+export const FORM_FIELD_DATATYPES = [
+    {
+        name: "String",
+        inputtype: "text",
+        inputoptions: {}
+    },
+    {
+        name: "Number",
+        inputtype: "number",
+        inputoptions: {
+            inputMode: "numeric"
+        }
+    },
+    {
+        name: "Email",
+        inputtype: "email",
+        inputoptions: {}
+    },
+    {
+        name: "Contact-number",
+        inputtype: "text",
+        inputoptions: {
+            inputMode: "numeric",
+            maxLength: 10
+        }
+    },
+    {
+        name: "Datetime",
+        inputtype: "datetime-local",
+        inputoptions: {}
+    },
+    {
+        name: "File",
+        inputtype: "file",
+        inputoptions: {}
+    },
+    {
+        name: "Boolean",
+        inputtype: "radio",
+        inputoptions: {},
+        isEnum: true
+    },
+    {
+        name: "Enum",
+        inputtype: "",
+        inputoptions: {},
+        isEnum: true
+    }
+]
