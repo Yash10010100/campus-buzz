8ea80b175e01495aa19e711f00a26f9f07d32919
Yash <yashpadhiyar.pyr@gmail.com>
2025-07-09 20:25:45 +0800

    Add new routes to events router

--- api/src/routes/event.routes.js
+++ api/src/routes/event.routes.js
@@ -7,6 +7,12 @@ import {
     changeThemeImage,
     getEvent,
     deleteEvent,
+    fetchFutureEvents,
+    searchEventsWithQuery,
+    futureEventsByOrg,
+    pastEventsByOrg,
+    currentStudentParticipationEvents,
+    pastParticipationEvents
 } from "../controllers/event.controller.js";
 
 const router = Router()
@@ -23,5 +29,17 @@ router.route("/:eventId")
 router.route("/:eventId/theme-image").patch(changeThemeImage)
 
 router.route("/:eventId/form").post(createRegistrationForm)
+
+router.route("/fetch")
+.get(fetchFutureEvents)
+.post(searchEventsWithQuery)
+
+router.route("/fetch/org").get(futureEventsByOrg)
+
+router.route("/fetch/org-history").get(pastEventsByOrg)
+
+router.route("/fetch/std").get(currentStudentParticipationEvents)
+
+router.route("/fetch/std-history").get(pastParticipationEvents)
 
 export default router
